46f462db4a503686affa87dc771f9c93c25f4f3d
irian <irian.code@hotmail.com>
2024-06-18 17:29:02 +0200

    Improved querying

--- nextjs-app/src/app/api/send-prompt/llm/Agent.ts
+++ nextjs-app/src/app/api/send-prompt/llm/Agent.ts
@@ -1,3 +1,4 @@
+import {SystemMessage} from '@langchain/core/messages';
 import {ChatPromptTemplate} from '@langchain/core/prompts';
 import {ChatOpenAI} from '@langchain/openai';
 import {embedPDF, queryCollection} from '../vector-db/VectorDB';
@@ -15,31 +16,34 @@ export async function sendPrompt(prompt: string) {
 
   const collectionName = 'a-test-collection';
 
-  try {
-    await embedPDF('http://localhost:3000/test-pdf.pdf', collectionName);
-  } catch (error) {
-    console.error(error);
-  }
+  const retrievedContext = await (async () => {
+    const context = await queryCollection(collectionName, prompt);
 
-  const res = await queryCollection(collectionName, prompt);
+    if (context.length === 0) {
+      await embedPDF('http://localhost:3000/test-pdf.pdf', collectionName);
+      return await queryCollection(collectionName, prompt);
+    } else {
+      return context;
+    }
+  })();
 
-  const template = `Answer the user's question to the best of your ability.
-  Use the following fictitious story as context to answer.
-  
-  STORY: {context}
-  
+  console.log('heeey 2.4', {retrievedContext});
+
+  const chatText = `Use the following pieces of text from a fictitious story as context to answer the user's question to the best of your ability.
+  STORY FRAGMENTS: {context}
   USER QUESTION: {question}`;
 
-  const chatTemplate = ChatPromptTemplate.fromTemplate(template);
-
-  const finalPrompt = await chatTemplate.invoke({
-    context: res.map((doc) => doc.pageContent).join('\n\n'),
+  const chatTemplate = await ChatPromptTemplate.fromTemplate(chatText).invoke({
+    context: retrievedContext.map((doc) => doc.pageContent).join('\n\n'),
     question: prompt,
   });
 
-  console.log('heeey 2.5', finalPrompt.toString());
-
-  const response = await llm.invoke(finalPrompt.toChatMessages());
+  const response = await llm.invoke([
+    new SystemMessage(
+      "You're a helpful AI assistant expert in Bitcoin cryptocurrency. Your answers should be elaborate. If you don't know the answer just say 'I don't know'."
+    ),
+    ...chatTemplate.toChatMessages(),
+  ]);
 
   return response.content;
 }
