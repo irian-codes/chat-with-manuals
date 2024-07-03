8d626e545c4ee22627c2c9aca006e01bac0fcf4e
irian <irian.code@hotmail.com>
2024-07-03 23:24:56 +0200

    Created debug page and route

--- nextjs-app/src/app/api/debug/parse-pdf/route.ts
+++ nextjs-app/src/app/api/debug/parse-pdf/route.ts
@@ -1,19 +1,29 @@
-import {assert} from 'console';
 import {NextRequest, NextResponse} from 'next/server';
 import {parsePdf} from './functions';
 
 export async function POST(request: NextRequest) {
   const formData = await request.formData();
   const file = formData.get('pdf');
-  assert(file instanceof File, 'file is required');
-
   const output = formData.get('output')?.toString();
-  assert(typeof output === 'string', 'output is required and must be a string');
 
   try {
+    if (!(file instanceof File)) {
+      throw new Error('The provided file is invalid or missing.');
+    }
+
+    if (typeof output !== 'string' || output.trim().length === 0) {
+      throw new Error("The 'output' field is required and must be a string.");
+    }
+
     const result = await parsePdf(URL.createObjectURL(file), output);
+
     return NextResponse.json({result});
   } catch (error) {
-    return NextResponse.json({error}, {status: 500});
+    console.error(error);
+
+    return NextResponse.json(
+      {error: (error as Error)?.message ?? 'Unknown error'},
+      {status: 500}
+    );
   }
 }
---
+++ nextjs-app/src/app/debug/upload-pdf/page.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import {useState} from 'react';
+
+export default function UploadPDFPage() {
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleFileDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+
+    const files = event.dataTransfer.files;
+    if (files.length > 0) {
+      setFile(files[0]);
+      uploadFile(files[0]);
+    } else {
+      setFile(null);
+    }
+  };
+
+  async function uploadFile(file: File) {
+    const formData = new FormData();
+    formData.append('pdf', file);
+
+    try {
+      const response = await fetch('/api/debug/parse-pdf', {
+        method: 'POST',
+        body: formData,
+      });
+
+      const res = await response.json();
+
+      if (!response.ok) {
+        throw new Error(
+          `Failed to upload PDF: ${response.status} - ${response.statusText}
+          ${JSON.stringify(res, null, 2)}`
+        );
+      }
+
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+
+      alert(
+        'Error: ' + (error instanceof Error ? error.message : String(error))
+      );
+    }
+  }
+
+  return (
+    <div
+      className="flex min-h-screen flex-col items-center justify-start p-24"
+      onDrop={handleFileDrop}
+      onDragOver={(event) => event.preventDefault()}
+    >
+      <h1 className="font-sans text-4xl">Upload PDF</h1>
+
+      {!file && (
+        <div className="mt-6 flex h-64 w-64 items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-black">
+          <p className="text-center text-xl">DRAG TO UPLOAD FILE</p>
+        </div>
+      )}
+
+      {file && (
+        <div className="mt-6 flex items-center justify-center">
+          <p className="m-6">
+            {file.name} ({(file.size / (1024 * 1024)).toFixed(2)} MB)
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
