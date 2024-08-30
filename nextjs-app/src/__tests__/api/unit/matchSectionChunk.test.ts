dd263ca4d3aa1953a83ab3fec9bcc5b8598f8ae3
irian <irian.code@hotmail.com>
2024-08-30 18:12:08 +0200

    Fixed missing ids bug

--- nextjs-app/src/__tests__/api/unit/matchSectionChunk.test.ts
+++ nextjs-app/src/__tests__/api/unit/matchSectionChunk.test.ts
@@ -7,6 +7,7 @@ import {describe, expect, it} from 'vitest';
 describe('matchSectionChunk', () => {
   it('should find the correct clear match. Easy difficulty.', async () => {
     const easyMatchSectionChunk: SectionChunkDoc = new Document({
+      id: 'S1',
       pageContent: 'The quick brown fox jumps over the lazy dog.',
       metadata: {
         headerRoute: '1>1',
@@ -21,14 +22,17 @@ describe('matchSectionChunk', () => {
 
     const easyMatchLayoutChunks: TextChunkDoc[] = [
       new Document({
+        id: 'L1',
         pageContent: 'A different sentence.',
         metadata: {totalOrder: 20, tokens: 3, charCount: 19},
       }),
       new Document({
+        id: 'L2',
         pageContent: 'The quick brown fox jumps over the lazy dog.',
         metadata: {totalOrder: 55, tokens: 9, charCount: 44},
       }),
       new Document({
+        id: 'L3',
         pageContent:
           'The red car was speeding so much it hit the side of the road!',
         metadata: {totalOrder: 80, tokens: 15, charCount: 61},
@@ -47,6 +51,7 @@ describe('matchSectionChunk', () => {
 
   it('should find the correct clear match. Medium difficulty.', async () => {
     const mediumMatchSectionChunk: SectionChunkDoc = new Document({
+      id: 'S1',
       pageContent: 'The quick brown fox.',
       metadata: {
         headerRoute: '1>1',
@@ -61,14 +66,17 @@ describe('matchSectionChunk', () => {
 
     const mediumMatchLayoutChunks: TextChunkDoc[] = [
       new Document({
+        id: 'L1',
         pageContent: 'The quick green dog.',
         metadata: {totalOrder: 55, tokens: 5, charCount: 20},
       }),
       new Document({
+        id: 'L2',
         pageContent: 'The quick brown fox.',
         metadata: {totalOrder: 56, tokens: 5, charCount: 20},
       }),
       new Document({
+        id: 'L3',
         pageContent: 'A quick brown animal.',
         metadata: {totalOrder: 57, tokens: 5, charCount: 21},
       }),
@@ -84,6 +92,7 @@ describe('matchSectionChunk', () => {
 
   it('should find the correct clear match. High difficulty.', async () => {
     const hardMatchSectionChunk: SectionChunkDoc = new Document({
+      id: 'S1',
       pageContent: 'The quick brown fox',
       metadata: {
         headerRoute: '1>1',
@@ -98,14 +107,17 @@ describe('matchSectionChunk', () => {
 
     const hardMatchLayoutChunks: TextChunkDoc[] = [
       new Document({
+        id: 'L1',
         pageContent: 'The quick brown fox',
         metadata: {totalOrder: 51, tokens: 4, charCount: 19},
       }),
       new Document({
+        id: 'L2',
         pageContent: 'The quick brown fo',
         metadata: {totalOrder: 52, tokens: 4, charCount: 18},
       }),
       new Document({
+        id: 'L3',
         pageContent: 'The quick brown dog',
         metadata: {totalOrder: 53, tokens: 4, charCount: 19},
       }),
@@ -121,6 +133,7 @@ describe('matchSectionChunk', () => {
 
   it('should find the closest match when no clear match is found.', async () => {
     const sectionChunk: SectionChunkDoc = new Document({
+      id: 'S1',
       pageContent: 'The quick brown fox',
       metadata: {
         headerRoute: '1>1',
@@ -135,14 +148,17 @@ describe('matchSectionChunk', () => {
 
     const inexactLayoutChunks: TextChunkDoc[] = [
       new Document({
+        id: 'L1',
         pageContent: 'The quick brown dog',
         metadata: {totalOrder: 51, tokens: 4, charCount: 19},
       }),
       new Document({
+        id: 'L2',
         pageContent: 'The happy penguin',
         metadata: {totalOrder: 52, tokens: 4, charCount: 17},
       }),
       new Document({
+        id: 'L3',
         pageContent: 'The very long and sturdy building',
         metadata: {totalOrder: 53, tokens: 6, charCount: 33},
       }),
