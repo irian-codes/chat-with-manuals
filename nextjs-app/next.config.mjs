3c77e415ff21ec20c38385e7d7f5496f493bb98a
irian <irian.code@hotmail.com>
2024-10-11 19:01:42 +0200

    Ignoring module instead

--- nextjs-app/next.config.mjs
+++ nextjs-app/next.config.mjs
@@ -1,25 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, {isServer}) => {
-    // Needed for webpack to know how to load '.node' files from 'chromadb' dependency.
-    // See issue: https://github.com/chroma-core/chroma/issues/1178
-    config.module.rules.push({
-      test: /\.node$/,
-      use: 'node-loader',
-    });
+    // Ignoring this module for the browser since it's not needed and
+    // causes issues:
+    // See https://huggingface.co/docs/transformers.js/tutorials/next
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'onnxruntime-node$': false,
+    };
 
     return config;
   },
   experimental: {
-    // TODO: I hope this loader is compatible with Turbopack someday.
-    // For now it isn't. Check on:
-    // https://github.com/vercel/turborepo/issues/4265
-    //
-    // turbo: { rules: {
-    //   '*.node': { loaders: ['node-loader'],
-    //     },
-    //   },
-    // },
+    turbo: {
+      resolveAlias: {
+        'onnxruntime-node': {browser: ''},
+      },
+    },
   },
 };
 
