c9b073e91ce2895e4b8113b4b4bf185d595ff6bd
irian <irian.code@hotmail.com>
2024-11-15 17:09:49 +0100

    feat(lang): added initial localization data and set up i18nAlly

--- fullstack-app/next.config.js
+++ fullstack-app/next.config.js
@@ -8,13 +8,8 @@ import './src/env.js';
 const config = {
   reactStrictMode: true,
 
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
   i18n: {
-    locales: ['en'],
+    locales: ['en', 'es'],
     defaultLocale: 'en',
   },
   transpilePackages: ['geist'],
