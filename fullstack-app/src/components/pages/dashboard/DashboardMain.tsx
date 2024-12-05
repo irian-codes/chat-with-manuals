b5984b78f6a9f063da619b3f6a3e9cbd8aedfca0
irian <irian.code@hotmail.com>
2024-12-05 02:52:36 +0100

    fix(lang): flag not overlapping anymore with other elements
    Specially on small screens it was happening all the time.

--- fullstack-app/src/components/pages/dashboard/DashboardMain.tsx
+++ fullstack-app/src/components/pages/dashboard/DashboardMain.tsx
@@ -21,7 +21,7 @@ export function DashboardMain({documents}: DashboardProps) {
   return (
     <div className="flex-1">
       <Header>
-        <div className="relative max-w-md flex-1">
+        <div className="relative min-w-[250px] max-w-md flex-1">
           <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
           <Input placeholder={t('header.search')} className="pl-8" />
         </div>
--- fullstack-app/src/components/reusable/Header.tsx
+++ fullstack-app/src/components/reusable/Header.tsx
@@ -13,15 +13,17 @@ export function Header({children}: HeaderProps) {
   const showLanguageSwitcher = isCollapsed || isNotMobile;
 
   return (
-    <header className="border-b pr-12">
-      <div className="flex w-full flex-wrap items-center justify-between gap-4 p-4 pr-4">
-        {children}
-      </div>
-      {showLanguageSwitcher && (
-        <div className="absolute right-4 top-4">
-          <LanguageSwitcher />
+    <header className="border-b">
+      <div className="relative w-full">
+        <div className="flex flex-wrap items-center justify-between gap-4 p-4 pr-16">
+          {children}
         </div>
-      )}
+        {showLanguageSwitcher && (
+          <div className="absolute right-6 top-4 sm:right-4">
+            <LanguageSwitcher />
+          </div>
+        )}
+      </div>
     </header>
   );
 }
