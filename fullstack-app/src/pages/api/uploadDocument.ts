ecdec2d3c04cd832c64a87f617ceb5bf362f3e5f
irian <irian.code@hotmail.com>
2024-12-15 02:17:35 +0100

    fix(documents): checking for authorization first, it's more secure

--- fullstack-app/src/pages/api/uploadDocument.ts
+++ fullstack-app/src/pages/api/uploadDocument.ts
@@ -26,6 +26,24 @@ export default async function handler(
     return res.status(405).json({error: 'Method not allowed'});
   }
 
+  // Check authentication
+  const {userId: userAuthId} = getAuth(req);
+
+  if (!userAuthId) {
+    return res.status(401).json({error: 'Unauthorized'});
+  }
+
+  const dbUser = await db.user.findFirst({
+    where: {
+      authProviderId: userAuthId,
+    },
+  });
+
+  if (!dbUser) {
+    return res.status(401).json({error: 'Unauthorized'});
+  }
+
+  // Parse the form data
   const parsedFormData = {} as Omit<UploadDocumentPayload, 'file'> & {
     file: FileInfo;
   };
@@ -67,23 +85,6 @@ export default async function handler(
     console.error(zodResult.error);
 
     return res.status(400).json({error: 'Invalid request body'});
-  }
-
-  // Check authentication
-  const {userId: userAuthId} = getAuth(req);
-
-  if (!userAuthId) {
-    return res.status(401).json({error: 'Unauthorized'});
-  }
-
-  const dbUser = await db.user.findFirst({
-    where: {
-      authProviderId: userAuthId,
-    },
-  });
-
-  if (!dbUser) {
-    return res.status(401).json({error: 'Unauthorized'});
   }
 
   let fileUrl: string;
