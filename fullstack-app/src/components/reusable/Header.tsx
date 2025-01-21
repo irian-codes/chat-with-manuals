e3322ca2d5186134d09f66f00d8b379b71584d11
irian <irian.code@hotmail.com>
2025-01-21 22:19:18 +0100

    fix(sidebar): sidebar collapse behaviour between routes corrected

--- fullstack-app/src/components/reusable/Header.tsx
+++ fullstack-app/src/components/reusable/Header.tsx
@@ -1,5 +1,3 @@
-import {useSidebar} from '@/contexts/ConversationsSidebarContext';
-import {useTailwindBreakpoint} from '@/hooks/useTailwindBreakpoint';
 import {UserButton} from '@clerk/nextjs';
 import {type ReactNode} from 'react';
 import {LanguageSwitcher} from './LanguageSwitcher';
@@ -9,16 +7,12 @@ interface HeaderProps {
 }
 
 export function Header({children}: HeaderProps) {
-  const {isCollapsed} = useSidebar();
-  const isNotMobile = useTailwindBreakpoint('sm');
-  const showLanguageSwitcher = isCollapsed || isNotMobile;
-
   return (
     <header className="border-b">
       <div className="flex flex-nowrap items-center justify-between gap-4 overflow-hidden p-4">
         {children}
         <div className="flex flex-col gap-2 pr-4 sm:flex-row-reverse">
-          {showLanguageSwitcher && <LanguageSwitcher />}
+          <LanguageSwitcher />
           <UserButton />
         </div>
       </div>
