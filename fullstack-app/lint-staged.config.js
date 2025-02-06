d0a1e5a51ee44cbce55fc2c5d56c70690f22475d
irian <irian.code@hotmail.com>
2025-02-06 20:16:27 +0100

    chore(deps): updated commit and linting deps

---
+++ fullstack-app/lint-staged.config.js
@@ -0,0 +1,17 @@
+import path from 'node:path';
+
+const buildEslintCommand = (filePaths) =>
+  `next lint --file ${filePaths
+    .map((filePath) => path.relative(process.cwd(), filePath))
+    .join(' --file ')}`;
+
+/**
+ * @filename: lint-staged.config.js
+ * @type {import('lint-staged').Configuration}
+ */
+const config = {
+  '*.{ts,js,tsx,jsx,mdx}': ['prettier --write --cache', buildEslintCommand],
+  '*.json': ['prettier --write --cache'],
+};
+
+export default config;
